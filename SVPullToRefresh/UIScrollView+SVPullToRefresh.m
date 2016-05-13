c2045098587687ac7dfc9350b663846be58a9176
LawrenceHan <yangfei6565@163.com>
2016-05-13 14:57:20 +0800

    Added missing code.

--- SVPullToRefresh/UIScrollView+SVPullToRefresh.m
+++ SVPullToRefresh/UIScrollView+SVPullToRefresh.m
@@ -264,8 +264,13 @@ static char UIScrollViewPullToRefreshView;
 #pragma mark - Observing
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
-    if([keyPath isEqualToString:@"contentOffset"])
-        [self scrollViewDidScroll:[[change valueForKey:NSKeyValueChangeNewKey] CGPointValue]];
+    if([keyPath isEqualToString:@"contentOffset"]) {
+        CGPoint newPoint = [[change valueForKey:NSKeyValueChangeNewKey] CGPointValue];
+        //only excute when pull up
+        if (newPoint.y <= 0) {
+            [self scrollViewDidScroll:newPoint];
+        }
+    }
     else if([keyPath isEqualToString:@"contentSize"]) {
         [self layoutSubviews];
         
